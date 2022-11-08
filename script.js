f5976805abd68800baf6c1ffb011a0b5ee4bba50
Amal Jama <amal_839@hotmail.com>
2022-11-08 17:32:00 -0500

    completed function allowing computer to make a choice

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // script.js
 
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomNum = Math.floor(Math.random() * 3); 
+    return choices[randomNum];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+function game() {
+    //ask for player input
+
+    //call playRound
+}
+
+console.log(getComputerChoice());
