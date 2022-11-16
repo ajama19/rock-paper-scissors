cd1fa60f0c6078b02b3c42bce182983f076ef516
Amal Jama <amal_839@hotmail.com>
2022-11-15 23:50:27 -0500

    worked on game ui and added animations

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // script.js
 
+/* This code removes the introduction text once the animation has completed
+and makes game user interface appear */
+let intro = document.getElementById('introduction');
+intro.addEventListener('animationend', (event) => {
+    let gameUI = document.getElementById('interface');
+    intro.setAttribute('class', 'hidden');
+    gameUI.setAttribute('class', 'game-ui');
+});
+
+
+
+
+
+
+
+
+
+// functions for playing the game
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let randomNum = Math.floor(Math.random() * 3); 
@@ -18,35 +36,35 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    //play a 5 round game
-    for (let i = 0; i < 5; i++) {
-        let playerInput = prompt("Rock, paper, or scissors? Which will you choose?");
-        let computerInput = getComputerChoice();
-        let gameResult = playRound(playerInput, computerInput);
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     //play a 5 round game
+//     for (let i = 0; i < 5; i++) {
+//         let playerInput = prompt("Rock, paper, or scissors? Which will you choose?");
+//         let computerInput = getComputerChoice();
+//         let gameResult = playRound(playerInput, computerInput);
         
-        if (gameResult === "win") {
-            ++playerScore;
-            console.log("You won this round! :)");
-        } else if (gameResult === "lose") {
-            ++computerScore;
-            console.log("You lost this round :(");
-        } else if (gameResult === "tie") { 
-            ++computerScore;
-            ++playerScore;
-            console.log("It's a tie! o_o");
-        }
-    }
+//         if (gameResult === "win") {
+//             ++playerScore;
+//             console.log("You won this round! :)");
+//         } else if (gameResult === "lose") {
+//             ++computerScore;
+//             console.log("You lost this round :(");
+//         } else if (gameResult === "tie") { 
+//             ++computerScore;
+//             ++playerScore;
+//             console.log("It's a tie! o_o");
+//         }
+//     }
 
-    if (playerScore > computerScore) {
-        console.log("You WIN!!!!!!");
-    } else if (playerScore < computerScore) {
-        console.log("NOOOOOOO YOU LOST!");
-    } else if (playerScore === computerScore) {
-        console.log("It's a tie!")
-    }
-}
+//     if (playerScore > computerScore) {
+//         console.log("You WIN!!!!!!");
+//     } else if (playerScore < computerScore) {
+//         console.log("NOOOOOOO YOU LOST!");
+//     } else if (playerScore === computerScore) {
+//         console.log("It's a tie!")
+//     }
+// }
 
-game();
+// game();
